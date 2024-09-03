1a76d45c9da354e6502d92b8a93e0e2fe1fa9fb3
Vladimir Spásov Dimitrov <spasov.vladimir@gmail.com>
2024-09-03 16:10:27 +0200

    Initial Commit

---
+++ Traffic/TrafficApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficApp.swift
+//  Traffic
+//
+//  Created by Vladimir Spasov on 3/9/24.
+//
+
+import SwiftUI
+
+@main
+struct TrafficApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
